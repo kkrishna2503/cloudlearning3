914065f2326254180ff8efa439181b680c3da529
kkrishna2503 <kkrishnas2503@gmail.com>
2026-09-13 15:56:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from Azure app services")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from my Azure App Service!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
